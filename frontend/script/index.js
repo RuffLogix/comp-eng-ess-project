b3dcf2e91ffaeee7221ec18ec5a396ce62d4fe91
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-04-21 23:44:07 +0700

    update: create, join, & waiting room functions

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,31 +1,82 @@
 function usernameCheck(){
-    var username = document.querySelector("#username").value;
-    if(username.length != 0 && username != null){
-        // Redirect to another page within the same directory
+    const username = document.querySelector("#username").value;
+    if (username.length != 0 && username != null) {
         return true
     }else{
         alert("Please enter your username");
         return false;
     }
 }
-function partIdCheck(){
+
+function partIdCheck() {
     const partyId = document.querySelector("#joinId").value;
-            if(partyId.length != 0 & partyId != null){
-                // go to party Id session
-                console.log("Joining Session ( under developing )")
-                // window.location.href = window.location.origin + "/frontend/custom.html";
-            }else{
-                alert('Please enter "Ped Id"')
-            }
+    if (partyId.length !== 0 && partyId !== null) {
+        return true;
+    }else{
+        alert('Please enter "Ped Id"')
+        return false;
+    }    
 }
+
+// Create a new room
 document.querySelector("#createPed").addEventListener("click",() => {
     if(usernameCheck()){
-        window.location.href = window.location.origin + "/frontend/custom.html";
+        let username = document.querySelector("#username").value;
+        fetch(`${backendUrl}/api/room/create`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+            }),
+        }).then((res) => res.json()).then((data) => {
+            const { roomId, status } = data;
+
+            if (status !== "success") {
+                alert("Failed to create room");
+                return;
+            }
+            
+            localStorage.setItem("roomId", roomId);
+            localStorage.setItem("username", username);
+
+            window.location.href = window.location.origin + "/frontend/custom.html";
+        });
     }
 });
+
+// Join an existing room
 document.querySelector("#join").addEventListener("click",() => {
-    if(usernameCheck()){
-        partIdCheck();
+    if(usernameCheck() && partIdCheck()){
+        let username = document.querySelector("#username").value;
+        let roomId = document.querySelector("#joinId").value;
+
+        fetch(`${backendUrl}/api/room/join`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                roomId: roomId,
+            }),
+        }).then((res) => res.json()).then((data) => {
+            const { roomId, status } = data;
+
+            if (status !== "success") {
+                alert("Failed to join room");
+                return;
+            }
+
+            localStorage.setItem("roomId", roomId);
+            localStorage.setItem("username", username);
+
+            window.location.href = window.location.origin + "/frontend/custom.html";
+        });
     }
+});
+
+addEventListener("load", () => {
+    localStorage.clear();
 })
-// console.log(username.length)
